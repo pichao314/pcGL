df8b378683313eb3621f19722b37359e92765eeb
pichaoalw <pichao314@gmail.com>
2019-10-19 09:33:09 -0700

    rearranged

--- src/pcGL.cpp
+++ src/pcGL.cpp
@@ -1,16 +1,16 @@
 /*
 ===============================================================================
  Name        : pcGL.cpp
- Author      : $RJ
+ Author      : $PC
  Version     :
  Copyright   : $(copyright)
- Description : main definition
+ Description : 2D and 3D Graphic Engine
 ===============================================================================
 */
 
 #include <cr_section_macros.h>
 #include <NXP/crp.h>
-#include "LPC17xx.h"                        /* LPC17xx definitions */
+#include "LPC17xx.h"                        //LPC17xx definitions
 #include "ssp.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -26,18 +26,13 @@
 
 using namespace std;
 
-
-
 /* Be careful with the port number and location number, because
-
 some of the location may not exist in that port. */
 
 #define PORT_NUM            0
 
-
 uint8_t src_addr[SSP_BUFSIZE];
 uint8_t dest_addr[SSP_BUFSIZE];
-
 
 #define ST7735_TFTWIDTH 127
 #define ST7735_TFTHEIGHT 159
@@ -47,8 +42,6 @@ uint8_t dest_addr[SSP_BUFSIZE];
 #define ST7735_RAMWR 0x2C
 #define ST7735_SLPOUT 0x11
 #define ST7735_DISPON 0x29
-
-
 
 #define swap(x, y) {x = x + y; y = x - y; x = x - y ;}
 
@@ -72,303 +65,165 @@ uint8_t dest_addr[SSP_BUFSIZE];
 int _height = ST7735_TFTHEIGHT;
 int _width = ST7735_TFTWIDTH;
 
-
 void spiwrite(uint8_t c)
-
 {
-
- int pnum = 0;
-
- src_addr[0] = c;
-
- SSP_SSELToggle( pnum, 0 );
-
- SSPSend( pnum, (uint8_t *)src_addr, 1 );
-
- SSP_SSELToggle( pnum, 1 );
-
+    int pnum = 0;
+    src_addr[0] = c;
+    SSP_SSELToggle( pnum, 0 );
+    SSPSend( pnum, (uint8_t *)src_addr, 1 );
+    SSP_SSELToggle( pnum, 1 );
 }
-
-
 
 void writecommand(uint8_t c)
-
 {
-
- LPC_GPIO0->FIOCLR |= (0x1<<21);
-
- spiwrite(c);
-
+    LPC_GPIO0->FIOCLR |= (0x1<<21);
+    spiwrite(c);
 }
-
-
 
 void writedata(uint8_t c)
-
 {
-
- LPC_GPIO0->FIOSET |= (0x1<<21);
-
- spiwrite(c);
-
+    LPC_GPIO0->FIOSET |= (0x1<<21);
+    spiwrite(c);
 }
-
-
 
 void writeword(uint16_t c)
-
 {
-
- uint8_t d;
-
- d = c >> 8;
-
- writedata(d);
-
- d = c & 0xFF;
-
- writedata(d);
-
+    uint8_t d;
+    d = c >> 8;
+    writedata(d);
+    d = c & 0xFF;
+    writedata(d);
 }
-
-
 
 void write888(uint32_t color, uint32_t repeat)
-
 {
-
- uint8_t red, green, blue;
-
- int i;
-
- red = (color >> 16);
-
- green = (color >> 8) & 0xFF;
-
- blue = color & 0xFF;
-
- for (i = 0; i< repeat; i++) {
-
-  writedata(red);
-
-  writedata(green);
-
-  writedata(blue);
-
- }
-
+    uint8_t red, green, blue;
+    int i;
+    red = (color >> 16);
+    green = (color >> 8) & 0xFF;
+    blue = color & 0xFF;
+    for (i = 0; i< repeat; i++) {
+        writedata(red);
+        writedata(green);
+        writedata(blue);
+    }
 }
-
-
 
 void setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1)
-
 {
-
- writecommand(ST7735_CASET);
-
- writeword(x0);
-
- writeword(x1);
-
- writecommand(ST7735_RASET);
-
- writeword(y0);
-
- writeword(y1);
-
+    writecommand(ST7735_CASET);
+    writeword(x0);
+    writeword(x1);
+    writecommand(ST7735_RASET);
+    writeword(y0);
+    writeword(y1);
 }
-
 
 void fillrect(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint32_t color)
-
 {
-
- int16_t i;
-
- int16_t width, height;
-
- width = x1-x0+1;
-
- height = y1-y0+1;
-
- setAddrWindow(x0,y0,x1,y1);
-
- writecommand(ST7735_RAMWR);
-
- write888(color,width*height);
-
+int16_t i;
+int16_t width, height;
+width = x1-x0+1;
+height = y1-y0+1;
+setAddrWindow(x0,y0,x1,y1);
+writecommand(ST7735_RAMWR);
+write888(color,width*height);
 }
-
-
 
 void lcddelay(int ms)
-
 {
-
- int count = 24000;
-
- int i;
-
- for ( i = count*ms; i--; i > 0);
-
+    int count = 24000;
+    int i;
+    for ( i = count*ms; i--; i > 0);
 }
-
-
 
 void lcd_init()
-
 {
-
- int i;
- //printf("LCD Demo Begins!!!\n");
- cout << "LCD Demo Begins!!!\n";
- // Set pins P0.16, P0.21, P0.22 as output
- LPC_GPIO0->FIODIR |= (0x1<<16);
-
- LPC_GPIO0->FIODIR |= (0x1<<21);
-
- LPC_GPIO0->FIODIR |= (0x1<<22);
-
- // Hardware Reset Sequence
- LPC_GPIO0->FIOSET |= (0x1<<22);
- lcddelay(500);
-
- LPC_GPIO0->FIOCLR |= (0x1<<22);
- lcddelay(500);
-
- LPC_GPIO0->FIOSET |= (0x1<<22);
- lcddelay(500);
-
- // initialize buffers
- for ( i = 0; i < SSP_BUFSIZE; i++ )
- {
-
-   src_addr[i] = 0;
-   dest_addr[i] = 0;
- }
-
- // Take LCD display out of sleep mode
- writecommand(ST7735_SLPOUT);
- lcddelay(200);
-
- // Turn LCD display on
- writecommand(ST7735_DISPON);
- lcddelay(200);
-
+    int i;
+    //printf("LCD Demo Begins!!!\n");
+    cout << "LCD Demo Begins!!!\n";
+    // Set pins P0.16, P0.21, P0.22 as output
+    LPC_GPIO0->FIODIR |= (0x1<<16);
+    LPC_GPIO0->FIODIR |= (0x1<<21);
+    LPC_GPIO0->FIODIR |= (0x1<<22);
+    // Hardware Reset Sequence
+    LPC_GPIO0->FIOSET |= (0x1<<22);
+    lcddelay(500);
+    LPC_GPIO0->FIOCLR |= (0x1<<22);
+    lcddelay(500);
+    LPC_GPIO0->FIOSET |= (0x1<<22);
+    lcddelay(500);
+    // initialize buffers
+    for ( i = 0; i < SSP_BUFSIZE; i++ )
+    {
+        src_addr[i] = 0;
+        dest_addr[i] = 0;
+    }
+    // Take LCD display out of sleep mode
+    writecommand(ST7735_SLPOUT);
+    lcddelay(200);
+    // Turn LCD display on
+    writecommand(ST7735_DISPON);
+    lcddelay(200);
 }
-
-
-
 
 void drawPixel(int16_t x, int16_t y, uint32_t color)
-
 {
-
- if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height))
-
- return;
-
- setAddrWindow(x, y, x + 1, y + 1);
-
- writecommand(ST7735_RAMWR);
-
- write888(color, 1);
-
+    if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height))
+        return;
+    setAddrWindow(x, y, x + 1, y + 1);
+    writecommand(ST7735_RAMWR);
+    write888(color, 1);
 }
 
-
-
 /*****************************************************************************
-
 * Descriptions:        Draw line function
-*
 * parameters:           Starting point (x0,y0), Ending point(x1,y1) and color
 * Returned value:        None
-*
 *****************************************************************************/
 void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint32_t color)
-
 {
-
- int16_t slope = abs(y1 - y0) > abs(x1 - x0);
-
- if (slope) {
-
-  swap(x0, y0);
-
-  swap(x1, y1);
-
- }
-
- if (x0 > x1) {
-
-  swap(x0, x1);
-
-  swap(y0, y1);
-
- }
-
- int16_t dx, dy;
-
- dx = x1 - x0;
-
- dy = abs(y1 - y0);
-
- int16_t err = dx / 2;
-
- int16_t ystep;
-
- if (y0 < y1) {
-
-  ystep = 1;
-
- }
-
- else {
-
-  ystep = -1;
-
- }
-
- for (; x0 <= x1; x0++) {
-
-  if (slope) {
-
-   drawPixel(y0, x0, color);
-
-  }
-
-  else {
-
-   drawPixel(x0, y0, color);
-
-  }
-
-  lcddelay(1);
-
-  err -= dy;
-
-  if (err < 0) {
-
-   y0 += ystep;
-
-   err += dx;
-
-  }
-
- }
-
+    int16_t slope = abs(y1 - y0) > abs(x1 - x0);
+    if (slope) {
+        swap(x0, y0);
+        swap(x1, y1);
+    }
+    if (x0 > x1) {
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+    int16_t dx, dy;
+    dx = x1 - x0;
+    dy = abs(y1 - y0);
+    int16_t err = dx / 2;
+    int16_t ystep;
+    if (y0 < y1) {
+        ystep = 1;
+    }
+    else {
+        ystep = -1;
+    }
+    for (; x0 <= x1; x0++) {
+        if (slope) {
+            drawPixel(y0, x0, color);
+        }
+        else {
+            drawPixel(x0, y0, color);
+        }
+        lcddelay(1);
+        err -= dy;
+        if (err < 0) {
+            y0 += ystep;
+            err += dx;
+        }
+    }
 }
 
 /*
 ===============================================================================
-
 The virtual - physical transform function
-
 ===============================================================================
 */
-
 
 int16_t v2px(float x)
 {
@@ -403,12 +258,9 @@ void drawFLine(float x0, float y0, float x1, float y1, uint32_t color)
     drawLine(v2px(x0), v2py(y0), v2px(x1), v2py(y1), color);
 }
 
-
 /*
 ===============================================================================
-
 The 2D point class
-
 ===============================================================================
 */
 
@@ -516,7 +368,6 @@ p2t p2t::shift(float x, float y)
 
 p2t::~p2t()
 {
-
 }
 
 void drawFLine(p2t start, p2t stop, uint32_t color)
@@ -529,12 +380,9 @@ void drawFLine(p2t start, p2t stop, uint32_t color)
     drawLine(v2px(x0), v2py(y0), v2px(x1), v2py(y1), color);
 }
 
-
 /*
 ===============================================================================
-
 The 2D polygon class
-
 ===============================================================================
 */
 
@@ -686,10 +534,6 @@ p3t::~p3t()
 {
 }
 
-
-
-
-
 /*
 ===============================================================================
 
@@ -826,7 +670,7 @@ void drawShadow()
 
 ===============================================================================
 
-Homework 1
+HW1:Rotate Square Screen Saver
 
 ===============================================================================
 
@@ -882,12 +726,11 @@ void rotateSquare()
 
 ===============================================================================
 
-Lab 1 Foreset
+Lab 1:Foreset
 
 ===============================================================================
 
 */
-
 
 //draw the initial trunk with brown
 void drawTrunk(p2t start, p2t stop)
@@ -919,8 +762,6 @@ void drawTrunk(p2t start, p2t stop)
 }
 
 //A branch class
-
-//-----------------------------------------------------------------------------
 
 class branch
 {
@@ -996,8 +837,6 @@ branch::branch(const branch& br){
 branch::~branch()
 {
 }
-
-//-----------------------------------------------------------------------------
 
 //Function to draw tree
 
@@ -1095,6 +934,7 @@ void drawForest(int num, int level)
     cout << "Stopped!!"<<endl;
 }
 
+
 /*
 
 ===============================================================================
@@ -1104,6 +944,7 @@ Main Function
 ===============================================================================
 
 */
+
 
 int main (void)
 
